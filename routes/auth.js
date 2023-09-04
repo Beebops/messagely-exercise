d79604f6fbb61a05ca1ca92d8a44b7664717e366
Beth Carretta <bethcarretta@gmail.com>
2023-09-04 15:19:16 -0400

    finished User methods, added register route

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,16 @@
+const jwt = require('jsonwebtoken')
+const Router = require('express').Router
+const router = new Router()
+
+const User = require('../models/user')
+const { SECRET_KEY } = require('../config')
+const ExpressError = require('../expressError')
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +18,16 @@
  *
  *  Make sure to update their last-login!
  */
+
+router.post('/register', async function (req, res, next) {
+  try {
+    let { username } = await User.register(req.body)
+    let token = jwt.sign({ username }, SECRET_KEY)
+    User.updateLoginTimestamp(username)
+    return res.json({ token })
+  } catch (err) {
+    return next(err)
+  }
+})
+
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,18 @@
+const express = require('express')
+const router = new express.Router()
+const User = require('../models/user')
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
-
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
 
 /** GET /:username/to - get messages to user
  *
@@ -21,7 +23,6 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-
 
 /** GET /:username/from - get messages from user
  *
